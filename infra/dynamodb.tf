84b4b6cd8a9cda55ea5d643cf4abdb3dc80495b3
anoop-singh02 <anoop.panesar@hotmail.com>
2025-07-27 13:30:04 -0700

    fix: restore Lambda role & unblock pipeline

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,17 +1,17 @@
 resource "aws_dynamodb_table" "flight_status" {
   name         = "flight_status"
-  billing_mode = "PAY_PER_REQUEST"   
+  billing_mode = "PAY_PER_REQUEST"
 
-  hash_key  = "FlightId"   # Partition Key
-  range_key = "Timestamp"  # Sort Key
+  hash_key  = "FlightId"  # Partition Key
+  range_key = "Timestamp" # Sort Key
 
   attribute {
     name = "FlightId"
-    type = "S"             # string
+    type = "S" # string
   }
 
   attribute {
     name = "Timestamp"
-    type = "S"             
+    type = "S"
   }
 }
